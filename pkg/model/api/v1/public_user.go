f6527c291a8375c4875d17c3e392f859d6302ff5
PeterYurkovich <pyurkovi@redhat.com>
2025-11-13 13:47:31 -0500

    [IGNORE] remove externalK8sProvider on PublicUserSpec
    Signed-off-by: PeterYurkovich <pyurkovi@redhat.com>

--- pkg/model/api/v1/public_user.go
+++ pkg/model/api/v1/public_user.go
@@ -23,11 +23,10 @@ type PublicNativeProvider struct {
 }
 
 type PublicUserSpec struct {
-	FirstName           string               `json:"firstName,omitempty" yaml:"firstName,omitempty"`
-	LastName            string               `json:"lastName,omitempty" yaml:"lastName,omitempty"`
-	NativeProvider      PublicNativeProvider `json:"nativeProvider,omitempty" yaml:"nativeProvider,omitempty"`
-	OauthProviders      []OAuthProvider      `json:"oauthProviders,omitempty" yaml:"oauthProviders,omitempty"`
-	ExternalK8sProvider bool                 `json:"externalK8sProvider,omitempty" yaml:"externalK8sProvider,omitempty"`
+	FirstName      string               `json:"firstName,omitempty" yaml:"firstName,omitempty"`
+	LastName       string               `json:"lastName,omitempty" yaml:"lastName,omitempty"`
+	NativeProvider PublicNativeProvider `json:"nativeProvider,omitempty" yaml:"nativeProvider,omitempty"`
+	OauthProviders []OAuthProvider      `json:"oauthProviders,omitempty" yaml:"oauthProviders,omitempty"`
 }
 
 func NewPublicUserSpec(u UserSpec) PublicUserSpec {
@@ -37,8 +36,7 @@ func NewPublicUserSpec(u UserSpec) PublicUserSpec {
 		NativeProvider: PublicNativeProvider{
 			Password: secret.Hidden(u.NativeProvider.Password),
 		},
-		OauthProviders:      u.OauthProviders,
-		ExternalK8sProvider: false,
+		OauthProviders: u.OauthProviders,
 	}
 }
 
